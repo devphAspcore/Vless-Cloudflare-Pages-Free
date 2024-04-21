fcbab4a742bc5fdfc91d4aea917e4239d075ac9a
devphAspcore <157202441+devphAspcore@users.noreply.github.com>
2024-04-22 01:47:33 +0330

    Update _worker.js
    proxy-ip-updated

--- _worker.js
+++ _worker.js
@@ -4,15 +4,20 @@ import { connect } from 'cloudflare:sockets';
 let userID = '47e8011b-65fa-4b76-8a16-5af2783c3970';
 
 const proxyIPs = [
-    "23.162.136.169",
-    "8.222.128.131",
-    "43.157.17.4",
-    "8.219.247.203",
-    "8.222.193.208",
-    "143.47.227.23",
-    "8.219.12.152",
-    "8.219.201.174",
-    "8.222.138.164"]; // ['23.162.136.169','cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org' , [2001:67c:2b0:db32:0:1:681a:404];
+	155.248.196.218,
+	8.219.210.64
+    ];
+
+// ['23.162.136.169','cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org' , [2001:67c:2b0:db32:0:1:681a:404];
+    //"23.162.136.169",
+   // "8.222.128.131",
+  //  "43.157.17.4",
+  //  "8.219.247.203",
+   // "8.222.193.208",
+   // "143.47.227.23",
+  //  "8.219.12.152",
+  //  "8.219.201.174",
+  //  "8.222.138.164"
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://cloudflare-dns.com/dns-query'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query or https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg=
