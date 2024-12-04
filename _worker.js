28272e8320298b9775cc38366f40c6824f5e4959
devphAspcore <157202441+devphAspcore@users.noreply.github.com>
2024-12-04 23:29:31 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 let userID = '47e8011b-65fa-4b76-8a16-5af2783c3970';
 
 // ['23.162.136.169','cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org' , [2001:67c:2b0:db32:0:1:681a:404];
-const proxyIPs = ["155.248.196.218","8.219.210.64","23.162.136.169","143.47.227.23"];
+const proxyIPs = ['23.162.136.169','cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org' , [2001:67c:2b0:db32:0:1:681a:404];//["155.248.196.218","8.219.210.64","23.162.136.169","143.47.227.23"];
    
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
